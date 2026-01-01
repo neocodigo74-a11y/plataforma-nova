c04b8f4acb2eac94b3cd588477b962885d1c8b8e
osvanio <osvaniosilva74@gmail.com>
2026-01-01 17:54:43 +0100

    versão atualizada 3

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,11 @@
-// lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  // Isso ajudará você a ver o erro nos logs do build sem travar tudo imediatamente
+  console.warn("Atenção: Variáveis de ambiente do Supabase não encontradas.");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
